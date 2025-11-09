4b4a5184e4970e8e06bb02c56008d4e65e0ae36e
a_pc <81596206+gdt2k@users.noreply.github.com>
2025-11-08 21:55:23 -0500

    Add Alt+Scroll to dot notes
    Kind of janky needs review

--- Assets/__Scripts/Editor/Input/BeatmapNoteInputController.cs
+++ Assets/__Scripts/Editor/Input/BeatmapNoteInputController.cs
@@ -146,6 +146,13 @@ public class BeatmapNoteInputController : BeatmapInputController<NoteContainer>,
             ? cutDirectionMovedBackward : cutDirectionMovedForward)[note.NoteData.CutDirection];
         BeatmapObjectContainerCollection.GetCollectionForType<NoteGridContainer>(ObjectType.Note)
             .RefreshSpecialAngles(note.ObjectData, false, false);
+        if(note.NoteData.CutDirection == (int)NoteCutDirection.Any)
+        {
+            note.NoteData.AngleOffset += (shiftForward ^ Settings.Instance.InvertScrollNoteAngle)
+                ? 45
+                : -45;
+            note.NoteData.AngleOffset %= 360;
+        }
 
         var actions = new List<BeatmapAction> { new BeatmapObjectModifiedAction(note.ObjectData, note.ObjectData, original, "Update Note Direction", mergeType: ActionMergeType.NoteDirectionChange) };
         CommonNotePlacement.UpdateAttachedSlidersDirection(note.NoteData, actions);
