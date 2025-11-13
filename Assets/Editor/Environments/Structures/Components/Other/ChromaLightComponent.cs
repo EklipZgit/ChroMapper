c1d13c647a8a3e0fe7911d6a989839bae5d8c33f
UGEcko <vanhelsing314@outlook.com>
2025-11-13 03:54:24 -0500

    Small Changes
    Just a few changes some of which include:
    Modulate EnvironmentInfo to its own assembly (affected parent folder scripts? will look into in the future)
    Switched EnvData and EnvInfo classes.
    Initialized EnvironmentComponent on some lightable components.

--- Assets/Editor/Environments/Structures/Components/Other/ChromaLightComponent.cs
+++ Assets/Editor/Environments/Structures/Components/Other/ChromaLightComponent.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// Stores Chroma light data from a lightable object.
+/// </summary>
 public class ChromaLightComponent
 {
     public int Type = -1;
--- Assets/Editor/Environments/Structures/EnvironmentData.cs
+++ Assets/Editor/Environments/Structures/EnvironmentData.cs
@@ -1,86 +1,16 @@
-using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 /// <summary>
-/// Metadata about the environment, including its name, internal ID, and color scheme.
+/// Main class for EnvData information. This class, and all its children, is structured according to the JSON schema used by EnvironmentData:
+/// https://ugecko.github.io/chroodleWeb/data/envdata/#__tabbed_1_5
 /// </summary>
+
 public class EnvironmentData
 {
-    [JsonProperty("environmentTitle")]
-    public string Title;
+    [JsonProperty("environmentData")]
+    public EnvironmentInfo Data;
 
-    [JsonProperty("environmentID")]
-    public string ID;
-
-    //[JsonProperty("colorScheme")]
-    [JsonIgnore]
-    public PlatformColorScheme ColorScheme;
-
-    [JsonProperty("fogParams")] 
-    public EnvironmentFogDefinition FogParameters;
-    
-    [JsonProperty("lightTracks")] 
-    public LightTracksDefinition LightTracks;
-    
-    [JsonProperty("uniqueMaterials")] 
-    public EnvInfoMaterial[] UniqueMaterials;
-    
-    [JsonProperty("uniqueMeshes")]
-    public string[] UniqueMeshes;
-}
-
-public class EnvironmentFogDefinition
-{
-    public float Offset;
-    public float Height;
-    public float StartY;
-    public float Attenuation;
-}
-
-public class LightTracksDefinition
-{
-    [JsonProperty("eventTracks")] 
-    public List<BasicTrackDefinition> V2LightTracks;
-    
-    [JsonProperty("groupPages")]
-    public Dictionary<string, List<PageDefinition>> V3GroupPages;
-    
-    public class BasicTrackDefinition
-    {
-        [JsonProperty("trackName")]
-        public string TrackName = "";
-        [JsonProperty("eventType")]
-        public string EventType = "";
-        [JsonProperty("toolbarType")]
-        public string ToolbarType = "";
-        [JsonProperty("page")]
-        public string Page = "";
-    }
-
-    public class PageDefinition
-    {
-        [JsonProperty("groupName")]
-        public string GroupName = "";
-        [JsonProperty("colorTrack")]
-        public bool ColorTrack = false;
-        [JsonProperty("floatFxTrack")]
-        public bool FloatFxTrack = false;
-        [JsonProperty("duplicate")]
-        public bool Duplicate = false;
-        
-        [JsonProperty("rotationTracks")]
-        public List<string> RotationTracks = new();
-        [JsonProperty("overrideDefaultRotationAxis")]
-        public string OverrideDefaultRotationAxis = "";
-        [JsonProperty("translationTracks")]
-        public List<string> TranslationTracks = new();
-        [JsonProperty("overrideDefaultTranslationAxis")]
-        public string OverrideDefaultTranslationAxis = "";
-    }
-}
-
-public class EnvInfoMaterial
-{
-    public string Name;
-    public string[] Keywords;
+    [JsonProperty("objects")]
+    public List<EnvironmentObject> Objects = new ();
 }
