62ed08c12a1147fea7c8340d57acd0a08e03104f
Caeden117 <caeden.s@outlook.com>
2026-03-19 23:47:21 -0700

    Standardize + optimize UI flyouts

--- Assets/__Scripts/UI/Editor/Chroma/ToggleColourDropdown.cs
+++ Assets/__Scripts/UI/Editor/Chroma/ToggleColourDropdown.cs
@@ -1,36 +1,24 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Serialization;
 
 public class ToggleColourDropdown : MonoBehaviour
 {
     [FormerlySerializedAs("ColourDropdown")] [SerializeField] private RectTransform colourDropdown;
-    public float YTop = 90;
-    public float YBottom = -50;
+    [SerializeField] private FlyoutPanelController flyoutPanelController;
+
     public bool Visible;
 
     public void ToggleDropdown(bool visible)
     {
-        gameObject.SetActive(true);
-        StopAllCoroutines();
         Visible = visible;
-        StartCoroutine(UpdateGroup(visible, colourDropdown));
-    }
 
-    private IEnumerator UpdateGroup(bool enabled, RectTransform group)
-    {
-        var dest = enabled ? YBottom : YTop;
-        var og = group.anchoredPosition.y;
-        float t = 0;
-        while (t < 1)
+        if (Visible)
         {
-            t += Time.deltaTime;
-            group.anchoredPosition = new Vector2(group.anchoredPosition.x, Mathf.Lerp(og, dest, t));
-            og = group.anchoredPosition.y;
-            yield return new WaitForEndOfFrame();
+            flyoutPanelController.Open();
         }
-
-        group.anchoredPosition = new Vector2(group.anchoredPosition.x, dest);
-        if (!enabled) group.gameObject.SetActive(false);
+        else
+        {
+            flyoutPanelController.Close();
+        }
     }
 }
