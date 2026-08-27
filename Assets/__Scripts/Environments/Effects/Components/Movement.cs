cc0451e89d11f78911e681a8de28810acb295226
Travis (EklipZ) <tdrake0x45@gmail.com>
2026-08-26 21:38:58 -0700

    Implement deterministic basic event movement previews

--- Assets/__Scripts/Environments/Effects/Components/Movement.cs
+++ Assets/__Scripts/Environments/Effects/Components/Movement.cs
@@ -2,47 +2,28 @@
 
 public class Movement : MonoBehaviour
 {
+    // Existing generated scenes retain this callback reference so the manager can
+    // migrate them to MovementEffect without regenerating every environment asset.
     public GenericCallbackEventEffect Effect;
 
     public Transform[] Transforms;
     public Vector3[] MovementData;
     public float TransitionSpeed;
 
-    private int currMovementIndex;
-    private Vector3 currPositionOffset;
-    private Vector3 prevPositionOffset;
     private Vector3[] startLocalPositions;
 
     private void Start()
     {
-        currPositionOffset = MovementData[0];
-        prevPositionOffset = currPositionOffset;
         startLocalPositions = new Vector3[Transforms.Length];
-        for (var i = 0; i < Transforms.Length; i++) startLocalPositions[i] = Transforms[i].localPosition;
-        SetLocalPositionOffsetsForAllObjects(currPositionOffset);
-        Effect.OnStateChanged += HandleStateChanged;
+        for (var i = 0; i < Transforms.Length; i++)
+            startLocalPositions[i] = Transforms[i].localPosition;
+
+        SetLocalPositionOffsetsForAllObjects(MovementData[0]);
     }
 
-    private void OnDestroy() => Effect.OnStateChanged -= HandleStateChanged;
-
-    protected void FixedUpdate()
+    public void Apply(Vector3 localPositionOffset)
     {
-        prevPositionOffset = currPositionOffset;
-        currPositionOffset = Vector3.LerpUnclamped(
-            currPositionOffset,
-            MovementData[currMovementIndex],
-            Time.fixedDeltaTime * TransitionSpeed);
-        if ((currPositionOffset - MovementData[currMovementIndex]).sqrMagnitude < 0.01f) enabled = false;
-    }
-
-    protected void LateUpdate() =>
-        SetLocalPositionOffsetsForAllObjects(
-            Vector3.LerpUnclamped(prevPositionOffset, currPositionOffset, TimeHelper.InterpolationFactor));
-
-    private void HandleStateChanged((int index, BasicEventStateData state) data)
-    {
-        currMovementIndex = data.index % MovementData.Length;
-        enabled = true;
+        SetLocalPositionOffsetsForAllObjects(localPositionOffset);
     }
 
     private void SetLocalPositionOffsetsForAllObjects(Vector3 localPositionOffset)
