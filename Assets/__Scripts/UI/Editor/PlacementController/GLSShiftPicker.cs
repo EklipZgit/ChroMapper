693de65a8d21a2afbd1badb06a4cdfc3cb95d455
Daniel Aspinwall <84806642+Vainstains@users.noreply.github.com>
2026-09-15 00:24:13 -0400

    fix shifting ui and get the popup working

---
+++ Assets/__Scripts/UI/Editor/PlacementController/GLSShiftPicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class GLSShiftPicker : MonoBehaviour
+{
+    [SerializeField] private GLSColorShiftArrayViewController colorShifts;
+    [SerializeField] private GLSColorShiftArrayViewController strobeColorShifts;
+    [SerializeField] private ButtonComponent clearButton;
+    [SerializeField] private ButtonComponent closeButton;
+    [SerializeField] private FlyoutPanelController flyout;
+
+    public event Action<string[]> OnShiftsChanged;
+    public event Action<string[]> OnStrobeShiftsChanged;
+
+    public string[] ColorShifts => colorShifts.GetShifts();
+    public string[] StrobeColorShifts => strobeColorShifts.GetShifts();
+
+    public void Open()
+    {
+        flyout.Open();
+    }
+
+    private void Awake()
+    {
+        clearButton.OnClick(HandleClearClicked);
+        colorShifts.Initialize(s => OnShiftsChanged?.Invoke(s));
+        strobeColorShifts.Initialize(s => OnStrobeShiftsChanged?.Invoke(s));
+        closeButton.OnClick(() => flyout.Close());
+    }
+
+    public void SetShifts(string[] color, string[] strobeColor)
+    {
+        if (color != null) colorShifts.SetShifts(color);
+        if (strobeColor != null) strobeColorShifts.SetShifts(strobeColor);
+    }
+
+    private void HandleClearClicked()
+    {
+        colorShifts.SetShifts(Array.Empty<string>());
+        strobeColorShifts.SetShifts(Array.Empty<string>());
+        OnShiftsChanged?.Invoke(Array.Empty<string>());
+        OnStrobeShiftsChanged?.Invoke(Array.Empty<string>());
+    }
+}
